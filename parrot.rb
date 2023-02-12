e7f7fd5a264d2f24940ff0d7cb2e829039c15a4d
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-12 15:09:09 +0300

    completed

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+    def parrot(my_parrot_str = "Squawk!")
+       puts my_parrot_str
+        return my_parrot_str
+    end
+    puts parrot
